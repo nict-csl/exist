7b757e35e8f20271e14bd077c0311442505c8377
Masato Jingu <jingu@nict.go.jp>
2018-12-04 14:32:09 +0900

    fix url

--- url/views.py
+++ url/views.py
@@ -34,19 +34,20 @@ class DetailView(TemplateView):
         url = self.kwargs['pk']
 
         response = self.getResponse(url)
-        context['response_sha256'] = self.gethash(response)
-        context['response_code'] = response.status_code
-        if "content-type" in response.headers:
-            context['content_type'] = response.headers["content-type"]
-        if "last-modified" in response.headers:
-            context['last_modified'] = response.headers["last-modified"]
-        if "server" in response.headers:
-            context['server'] = response.headers["server"]
-        if "content-length" in response.headers:
-            context['content_length'] = response.headers["content-length"]
-        context['title'] = self.gettitle(response)
-        context['imagefile'] = self.getimage(url)
-        context['websrc'] = self.getsrc(url)
+        if response is not None:
+            context['response_code'] = response.status_code
+            if "content-type" in response.headers:
+                context['content_type'] = response.headers["content-type"]
+                context['response_sha256'] = self.getHash(response)
+                context['title'] = self.getTitle(response)
+            if "last-modified" in response.headers:
+                context['last_modified'] = response.headers["last-modified"]
+            if "server" in response.headers:
+                context['server'] = response.headers["server"]
+            if "content-length" in response.headers:
+                context['content_length'] = response.headers["content-length"]
+        context['imagefile'] = self.getImage(url)
+        context['websrc'] = self.getSrc(url)
 
         vt = VT()
         context['vt_url'] = vt.getURLReport(url)
@@ -63,14 +64,14 @@ class DetailView(TemplateView):
         res.encoding = res.apparent_encoding
         return res
 
-    def gethash(self, res):
+    def getHash(self, res):
         if 'text/html' in res.headers["content-type"]:
             sha256 = hashlib.sha256(res.text.encode('utf-8')).hexdigest()
         else:
             sha256 = hashlib.sha256(res.content).hexdigest()
         return sha256
 
-    def gettitle(self, res):
+    def getTitle(self, res):
         title = ''
         if 'text/html' in res.headers["content-type"]:
             if '<title>' in res.text:
@@ -79,7 +80,7 @@ class DetailView(TemplateView):
                 title = res.text.split('<TITLE>')[1].split('</TITLE>')[0]
         return title
 
-    def getimage(self, url):
+    def getImage(self, url):
         imagehash = hashlib.md5(url.encode('utf-8')).hexdigest()
         filepath = "static/webimg/" + imagehash + ".png"
         if not os.path.exists(filepath):
@@ -87,7 +88,7 @@ class DetailView(TemplateView):
             subprocess.Popen(cmd, shell=True)
         return filepath
 
-    def getsrc(self, url):
+    def getSrc(self, url):
         imagehash = hashlib.md5(url.encode('utf-8')).hexdigest()
         filepath = "static/websrc/" + imagehash
         if not os.path.exists(filepath):
