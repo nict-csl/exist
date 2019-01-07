dc8542f90f62868db32b55116a85ad95ebec0cd8
Masato Jingu <jingu@nict.go.jp>
2019-01-07 18:23:36 +0900

    fix delete webdata script

--- scripts/url/delete_webdata.sh
+++ scripts/url/delete_webdata.sh
@@ -2,6 +2,6 @@
 
 source $(cd $(dirname $0); pwd)/url.conf
 
-find ${STATIC_DIR}/webimg/ -mtime +0| grep -v gitkeep| xargs rm -f
-find ${STATIC_DIR}/websrc/ -mtime +0| grep -v gitkeep| xargs rm -f
+find ${STATIC_DIR}/webimg/ -mtime +0 -type f| grep -v gitkeep| xargs rm -f
+find ${STATIC_DIR}/websrc/ -mtime +0 -type f| grep -v gitkeep| xargs rm -f
 
