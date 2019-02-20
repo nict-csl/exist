4f524c2a0bbbd6f66acf3525c644e07f4c799f3e
Masato Jingu <jingu@nict.go.jp>
2019-02-20 14:06:25 +0900

    delete unnecessary files

--- static/js/exist.js
+++ static/js/exist.js
@@ -8,8 +8,8 @@ $(function(){
         var $division = 5;
         $ele.find('.li').eq($division-1).after('<button class="morelink btn-sm btn-outline-info my-2">read more</button>');
         $ele.find('.li,.morelink').hide();
-        for(j=0;j<$division;j++){
-            $ele.find('.li').eq(j).show();
+        for(i=0;i<$division;i++){
+            $ele.find('.li').eq(i).show();
         }
         $ele.find('.morelink').show();
 
