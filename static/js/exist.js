5432adad5c2977bff73a0a615ddd27cf4cdcb7c1
Masato Jingu <jingu@nict.go.jp>
2019-02-20 13:39:39 +0900

    Merge branch 'my-readmore' into 'private'
    My readmore
    
    See merge request !1

--- static/js/exist.js
+++ static/js/exist.js
@@ -1,3 +1,21 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+$(function(){
+    $('more').each(function(){
+        var $ele = $(this);
+        var $division = 5;
+        $ele.find('.li').eq($division-1).after('<button class="morelink btn-sm btn-outline-info my-2">read more</button>');
+        $ele.find('.li,.morelink').hide();
+        for(j=0;j<$division;j++){
+            $ele.find('.li').eq(j).show();
+        }
+        $ele.find('.morelink').show();
+
+        $ele.find('.morelink').click(function(){
+            $ele.find('.li').fadeIn();
+            $ele.find('.morelink').hide();
+        });
+    });
+});
