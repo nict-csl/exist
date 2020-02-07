621321ebc67acdc20181afb55f960f29ce6ddd5b
nakochi <miho@kohhi.net>
2020-02-07 17:10:29 +0900

    celery.py: Change redis url to reading from system environment string

--- intelligence/celery.py
+++ intelligence/celery.py
@@ -3,6 +3,6 @@ from celery import Celery
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'intelligence.settings')
 app = Celery('intelligence')
-app.conf.broker_url = 'redis://localhost:6379/0'
+app.conf.broker_url = os.environ.get('EXIST_REDIS_URL', 'redis://localhost:6379/0')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
