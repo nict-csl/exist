2d4a566c0a1ec7c4ac258799f5a0dc6eb09c92f3
Masato Jingu <jingu@nict.go.jp>
2021-01-19 19:14:47 +0900

    Updated URL Lookup to responsive

--- apps/url/urls.py
+++ apps/url/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     url(r'^download/(?P<pk>\w{32})/$', views.getContents, name='getcontents'),
     url(r'^download/.+/$', views.IndexView.as_view()),
     url(r'^(?P<pk>.+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>.+)/get_vt$', views.get_context_vt, name='get_vt'),
 ]
 
